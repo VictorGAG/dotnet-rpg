bd1ed72d58dd5980a3b6e01ebd98d271b1c7ccec
VictorGAG <tblv@outlook.com.br>
2023-07-08 18:12:46 -0300

    feat: character service

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,9 @@
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        Task<List<Character>> GetAllCharacters();
+        Task<Character> GetCharacterById(int id);
+        Task<List<Character>> AddCharacter(Character newCharacter);
+    }
+}
